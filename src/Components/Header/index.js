21d007246211c4542f3ef5923ab4235431832495
Imy <imypullan@gmail.com>
2021-06-07 16:09:02 +0100

    adding normalize.css

---
+++ src/Components/Header/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './header.css'
+
+class Header extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <header>
+                <h1>BANK IT</h1>
+            </header>
+        )
+    }
+}
+
+
+export default Header

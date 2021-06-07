d1988ff4e37eb4fc8cac935cf3ae40816b25f135
Imy <imypullan@gmail.com>
2021-06-07 16:31:59 +0100

    basic header

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Header />
+      <div className="bankingAPIBody">
+          <Header />
+      </div>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
